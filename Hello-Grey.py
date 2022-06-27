dbbcba44772f41cd058235c0de95b89aa151bd4b
aggrey-Felix <aggreyfelix@yahoo.com>
2022-06-27 09:01:43 -0700

    first code in python,,amazing

---
+++ Hello-Grey.py
@@ -0,0 +1,5 @@
+"""what the heck is this ?
+Ugh!,,who does this,,probably every programmer who has ever wrote acode strted with this
+"""
+
+print("Hello, Grey")
---
+++ README.md
@@ -0,0 +1,1 @@
+# My 100 days of coding in Python started on 27thh of June 2022

3aae39d970d9c5308d16256d109974a181dbf9bd
aggrey-Felix <aggreyfelix@yahoo.com>
2022-06-27 09:13:56 -0700

    variable

---
+++ variable.py
@@ -0,0 +1,26 @@
+""" Python supports two types of numbers i.e Integer type number and float typt number and word type variable called strings and complex
+"""
+# 1. Integer shortened as int
+myint = 7
+print(myint)
+print(type(myint))
+
+#2. Float
+
+myfloat = 89.0
+print(myfloat)
+print(type(myfloat))
+
+"""
+3. strings
+Defined either in double or single quotes
+"""
+mystring = 'Hello'
+print(mystring)
+print(type(mystring)
+
+#including an apostrophe will propmt you use double quotes
+
+mystrings = "Don't you worry about a thing you are all sorted"
+print(mystrings)
+      

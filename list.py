71e7d3abbac2e1f505273e78dc465b4864a7f980
aggrey-Felix <aggreyfelix@yahoo.com>
2022-06-27 09:48:13 -0700

    basics about lists

---
+++ list.py
@@ -0,0 +1,17 @@
+#How to create a list
+
+mylist = [345, "cross", 5j]
+mylist.append(1)
+mylist.append("little")
+mylist.append(78.90)
+mylist.append("grey")
+print(mylist)
+for x in mylist:
+    print(x)
+
+#get the length of items in list
+print(len(mylist))
+
+#accessing an item using indexing
+myList = [123, 456, 678, "tool", 5j]
+print(myList[4])
